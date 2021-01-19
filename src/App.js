3c789ccbe55824cb806962b5e1c4ee34c30643fb
Vitor Ferraz <vitor_ferrazmaciel@hotmail.com>
2021-01-19 18:58:00 -0300

    Adicionando React

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Products from './components/Products';
+import Footer from './components/Footer';
+import Newsletter from './components/Newsletter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Banner />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
